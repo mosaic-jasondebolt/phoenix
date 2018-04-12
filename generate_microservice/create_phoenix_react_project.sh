34540c32ed943a46df8a93434ff0cc4127c4b07c
Jason DeBolt <jasondebolt@gmail.com>
2018-04-11 20:20:02 -0700

    Bootstrapping Phoenix with Phoenix!!

--- generate_microservice/create_phoenix_react_project.sh
+++ generate_microservice/create_phoenix_react_project.sh
@@ -1,8 +1,10 @@
+# Creates a Phoenix project
+
 aws cloudformation create-stack \
-    --stack-name phoenix-react \
+    --stack-name phoenix-phoenix \
     --template-body file://phoenix.json \
     --parameters \
-        ParameterKey=ProjectName,ParameterValue=phoenix-react \
+        ParameterKey=ProjectName,ParameterValue=phoenix \
         ParameterKey=NotificationEmail,ParameterValue=jason.debolt@joinmosaic.com \
-        ParameterKey=ProjectDescription,ParameterValue="The Mosaic Phoenix ReactJS starter project." \
+        ParameterKey=ProjectDescription,ParameterValue="The Mosaic Phoenix project." \
     --capabilities CAPABILITY_NAMED_IAM
