8b5579e01dddc807e9ef985cdc5e59a7434f7dd5
Jason DeBolt <jasondebolt@gmail.com>
2018-04-25 15:50:30 -0700

    Removing an the vpc prefix variable from the deploy-vpc.sh script.

--- aws/deploy-vpc.sh
+++ aws/deploy-vpc.sh
@@ -13,8 +13,6 @@ if [ $# -ne 1 ]
     exit 1
 fi
 
-VPC_STACK_NAME_PREFIX='microservice-vpc'
-
 # Regenerate the dev params file into a format the the CloudFormation CLI expects.
 python parameters_generator.py template-vpc-params-dev.json > temp_dev.json
 python parameters_generator.py template-vpc-params-testing.json > temp_testing.json
