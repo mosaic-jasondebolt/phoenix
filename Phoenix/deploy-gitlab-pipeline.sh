011e863108b7857dcd67f1ebe5dabc27268d9d7a
Jason DeBolt <jasondebolt@gmail.com>
2018-04-29 09:51:54 -0700

    Adding a CloudFormation template to create a GitLab to s3/CodePipeline.
    - You can read more about the original CloudFormation template here:
      https://aws.amazon.com/blogs/devops/integrating-git-with-aws-codepipeline/
    - I've updated the AWS blog template to work with Phoenix networking by
      adding a VPC, security groups, and subnets to the GitPull lambda function,
      and also by granting ec2: IAM permissions for the lambda function
      to be VPC enabled.
    - The AllowedIp address can be used to tell the API gateway which
      IP addresses a request can come from. If you have a private GitLab
      server behind a NAT instance or gateway, you can place the public IP
      of that gateway/instance in this field.
    - The API_SECRET is generated by the deploy script.
    - You must create a GitLab webhook with the generated API_SECRET,
      the SSH Key from the stack, and the API Gateway URL from the stack.

---
+++ Phoenix/deploy-gitlab-pipeline.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+# https://aws.amazon.com/blogs/devops/integrating-git-with-aws-codepipeline/
+# Deploys an API Gateway --> Lambda --> S3 pipeline for streaming master branch pushes to an S3 bucket.
+# 54.175.38.99/32 is the IP address of the NAT instance in front of our GitLab instance.
+#
+# USAGE:
+#  ./deploy-gitlab-pipeline.sh [create | update]
+
+
+# Check for valid arguments
+if [ $# -ne 1 ]
+  then
+    echo "Incorrect number of arguments supplied. Pass in either 'create' or 'update'."
+    exit 1
+fi
+
+# Extract JSON properties for a file into a local variable
+PROJECT_NAME=`jq -r '.Parameters.ProjectName' template-microservice-params.json`
+API_SECRET=`pwgen 32`
+
+# Replace the API_SECRET string in the dev params file with the $API_SECRET variable
+sed "s/API_SECRET/$API_SECRET/g" template-gitlab-pipeline-params.json > temp1.json
+
+python parameters_generator.py temp1.json > temp2.json
+
+# Validate the CloudFormation template before template execution.
+aws cloudformation validate-template --template-body file://template-gitlab-pipeline.json
+
+aws cloudformation $1-stack \
+    --stack-name $PROJECT_NAME-gitlab-pipeline \
+    --template-body file://template-gitlab-pipeline.json \
+    --parameters file://temp2.json \
+    --capabilities CAPABILITY_IAM
+
+# Cleanup
+rm temp1.json
+rm temp2.json
+
+echo ================ API SECRET ==================
+echo $API_SECRET
