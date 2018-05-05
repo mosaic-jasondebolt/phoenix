e134f3f7b2f82512319f981fd704fdc2b3244f26
Jason DeBolt <jasondebolt@gmail.com>
2018-05-05 16:40:39 -0700

    Adding a buildspec-dev.yml file for run simplified builds for pre-merge/code review only.
    - I decided to remove the deploy stages from the developer pipeline as it was getting a bit too complex.

--- Phoenix/deploy-code-pipeline-dev.sh
+++ Phoenix/deploy-code-pipeline-dev.sh
@@ -13,7 +13,7 @@ set -e
 # Extract JSON properties for a file into a local variable
 PROJECT_NAME=`jq -r '.Parameters.ProjectName' template-microservice-params.json`
 ENVIRONMENT=`jq -r '.Parameters.Environment' template-code-pipeline-params-dev.json`
-VERSION=`jq -r '.Parameters.Version' template-code-pipeline-params-dev.json`
+BRANCH_NAME=`jq -r '.Parameters.BranchName' template-code-pipeline-params-dev.json`
 # Allow developers to name the environment whatever they want, supporting multiple dev environments.
 
 # Check for valid arguments
@@ -30,7 +30,7 @@ python parameters_generator.py template-code-pipeline-params-dev.json cloudforma
 aws cloudformation validate-template --template-body file://template-code-pipeline.json
 
 # Create or update the CloudFormation stack with deploys your docker service to the Dev cluster.
-aws cloudformation $1-stack --stack-name $PROJECT_NAME-code-pipeline-$ENVIRONMENT-$VERSION \
+aws cloudformation $1-stack --stack-name $PROJECT_NAME-code-pipeline-$ENVIRONMENT-$BRANCH_NAME \
     --template-body file://template-code-pipeline.json \
     --parameters file://temp1.json \
     --capabilities CAPABILITY_IAM
