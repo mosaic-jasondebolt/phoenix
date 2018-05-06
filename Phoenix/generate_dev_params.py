9ed85f249436daff6319e3d52f285b7e7b14c8b4
Jason DeBolt <jasondebolt@gmail.com>
2018-05-05 18:38:25 -0700

    generate_dev_params.py param fix.

--- Phoenix/generate_dev_params.py
+++ Phoenix/generate_dev_params.py
@@ -92,7 +92,7 @@ def write_dev_param_files(environment_name):
 def main(args):
     if len(args) != 1:
         raise SystemExit('Invalid arguments!')
-    environment_name = args[1]
+    environment_name = args[0]
     write_dev_param_files(environment_name)
 
 if __name__ == '__main__':
