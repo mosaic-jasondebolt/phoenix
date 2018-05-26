9891b0c17d1f41e1d9e066bbd2c3c22d455822df
Jason DeBolt <jasondebolt@gmail.com>
2018-05-26 11:54:34 -0700

    Jenkins template is working now after pulling the ACM cert ARN from SSM.

--- Phoenix/deploy-jenkins.sh
+++ Phoenix/deploy-jenkins.sh
@@ -4,42 +4,48 @@ set -e
 # Deploys a Jenkins instance.
 
 # USAGE:
-#   ./deploy-jenkins.sh [create | update]
+#   ./deploy-jenkins.sh
+#
+# DO NOT try to update this stack as a volume snapshot may not be automatically created.
+# To update this stack, delete the current Jenkins stack, record the ID of the snapshot
+# that gets automatically saved, and use the snapshot ID in the 'snapshotId' stack
+# parameter before running this script to create a new jenkins stack retored with
+# the previous volume.
+#
+# To see how this looks, look at the DeletionPolicy of the volume in the jenkins template:
+#
+#     "VarLibJenkins": {
+#      "DeletionPolicy": "Snapshot",
+#       ...
 #
 # EXAMPLES:
-#   ./deploy-jenkins.sh create
-#   ./deploy-jenkins.sh update [version_id]
+#   ./deploy-jenkins.sh
 #
 #   Where,
 #     version_id is the number at the end of the stack name.
 
 # Extract JSON properties for a file into a local variable
-PROJECT_NAME=`jq -r '.Parameters.ProjectName' template-microservice-params.json`
-DOMAIN=`jq -r '.Parameters.Domain' template-microservice-params.json`
-DNS_PREFIX=`jq -r '.Parameters.DnsPrefix' template-jenkins-params.json`
+PROJECT_NAME=$(aws ssm get-parameter --name project-name | jq '.Parameter.Value' | sed -e s/\"//g)
+DNS_PREFIX=$(jq -r '.Parameters.DnsPrefix' template-jenkins-params.json)
 
 # Check for valid arguments
-if [ $# -ne 1 ];
+if [ $# -ne 0 ];
   then
-    echo "Incorrect number of arguments supplied. Pass in either 'create' or 'update'."
+    echo "Incorrect number of arguments supplied. This script takes no arguments"
     exit 1
 fi
 
-# Replace the VERSION_ID string in the params file with the $VERSION_ID variable
-sed "s/DOMAIN/$DOMAIN/g" template-jenkins-params.json > temp1.json
-
 # Regenerate the params file into a format the the CloudFormation CLI expects.
-python parameters_generator.py temp1.json cloudformation > temp2.json
+python parameters_generator.py template-jenkins-params.json cloudformation > temp1.json
 
 # Validate the CloudFormation template before template execution.
 aws cloudformation validate-template --template-body file://template-jenkins.json
 
 # Create or update the CloudFormation stack with deploys your docker service to the Dev cluster.
-aws cloudformation $1-stack --stack-name $PROJECT_NAME-jenkins-$DNS_PREFIX \
+aws cloudformation create-stack --stack-name $PROJECT_NAME-jenkins-$DNS_PREFIX \
     --template-body file://template-jenkins.json \
-    --parameters file://temp2.json \
+    --parameters file://temp1.json \
     --capabilities CAPABILITY_IAM
 
 # Cleanup
 rm temp1.json
-rm temp2.json
--- Phoenix/deploy-microservice.sh
+++ Phoenix/deploy-microservice.sh
@@ -14,21 +14,14 @@ if [ $# -ne 1 ]
 fi
 
 # Extract JSON properties for a file into a local variable
-PROJECT_NAME=`jq -r '.Parameters.ProjectName' template-ssm-parameters-params.json`
+PROJECT_NAME=$(aws ssm get-parameter --name project-name | jq '.Parameter.Value' | sed -e s/\"//g)
 MICROSERVICE_STACK_NAME=$PROJECT_NAME-microservice
-MICROSERVICE_BUCKET_NAME=`jq -r '.Parameters.MicroserviceBucketName' template-ssm-parameters-params.json | sed 's/PROJECT_NAME/'$PROJECT_NAME'/g'`
-VERSION_ID=`date +"%Y-%m-%d-%H%M%S"`
+MICROSERVICE_BUCKET_NAME=$(aws ssm get-parameter --name microservice-bucket-name | jq '.Parameter.Value' | sed -e s/\"//g)
 
 # Generate the MICROSERVICE bucket if it doesn't already exist
 aws s3 mb s3://$MICROSERVICE_BUCKET_NAME
 aws s3 sync . s3://$MICROSERVICE_BUCKET_NAME/cloudformation --exclude "*" --include "template-*.json" --delete
 aws s3 sync ./lambda s3://$MICROSERVICE_BUCKET_NAME/lambda/ --include "*" --delete
-
-# Replace the IMAGE_TAG string in the dev params file with the $IMAGE_TAG variable
-sed "s/VERSION_ID/$VERSION_ID/g" template-microservice-params.json > temp1.json
-
-# Regenerate the dev params file into a format the the CloudFormation CLI expects.
-python parameters_generator.py temp1.json cloudformation > temp2.json
 
 # Validate the CloudFormation template before template execution.
 aws cloudformation validate-template --template-url https://s3.amazonaws.com/$MICROSERVICE_BUCKET_NAME/cloudformation/template-microservice.json
@@ -36,9 +29,4 @@ aws cloudformation validate-template --template-url https://s3.amazonaws.com/$MI
 aws cloudformation $1-stack \
     --stack-name $MICROSERVICE_STACK_NAME \
     --template-url https://s3.amazonaws.com/$MICROSERVICE_BUCKET_NAME/cloudformation/template-microservice.json \
-    --parameters file://temp2.json \
     --capabilities CAPABILITY_NAMED_IAM
-
-# Cleanup
-rm temp1.json
-rm temp2.json
