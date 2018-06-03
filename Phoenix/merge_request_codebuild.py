b853f0ab644f5cfe6d3b96502ea4d7092fa0885a
Jason DeBolt <jasondebolt@gmail.com>
2018-06-02 21:20:56 -0700

    Refactored merge_request_codebuild.py script

--- Phoenix/merge_request_codebuild.py
+++ Phoenix/merge_request_codebuild.py
@@ -1,14 +1,13 @@
 """ Handles CodeBuild jobs executing in the context of a GitLab Merge Request
 
 USAGE:
-  python2.7 merge_request_codebuild.py
+  python2.7 merge_request_codebuild.py [build | unit-test | lint]
 
   This script must be run using Python2.7 as that is the version of Python
   that is being used in our AWS CodeBuild environment.
 
   As long as you have the environment variables set as specified in the
-  'isMergeRequest' function, you can run this script either locally
-  or on AWS CodeBuild.
+  initializer, you can run this script either locally or on AWS CodeBuild.
 
   Within AWS CodeBuild, these environment variables will automatically be set
   from the 'template-merge-request-pipeline.json' CloudFormation stack, which
@@ -29,74 +28,71 @@ import sys
 import json
 import requests
 
-def isMergeRequest():
-    # Notify gitlab of successful build status if these environment variables are present
-    # Some of these env variables will be made available to the CodeBuild job via
-    # CloudFormation environment variable injection
-    return (
-        os.environ.get('GITLAB_ACCESS_TOKEN') and
-        os.environ.get('PROJECT_NAME') and
-        os.environ.get('PIPELINE_NAME') and
-        os.environ.get('GITLAB_PROJECT_ID') and
-        os.environ.get('MERGE_REQUEST_INTERNAL_ID') # This env variable is inject via cloudformation into the CodeBuild job
-    )
+# These env variables are passed in via CloudFormation
+PIPELINE_NAME = os.environ.get('PIPELINE_NAME')
+GITLAB_PROJECT_ID = os.environ.get('GITLAB_PROJECT_ID')
+MERGE_REQUEST_INTERNAL_ID = os.environ.get('MERGE_REQUEST_INTERNAL_ID')
 
-def _get_code_build_url(region, build_name, build_id):
+# These env variables are sometimes made avaialable in codebuild
+GITLAB_ACCESS_TOKEN = os.environ.get('GITLAB_ACCESS_TOKEN')
+GITLAB_UNIT_TEST_ACCESS_TOKEN = os.environ.get('GITLAB_UNIT_TEST_ACCESS_TOKEN')
+GITLAB_LINT_ACCESS_TOKEN = os.environ.get('GITLAB_LINT_ACCESS_TOKEN')
+
+# These env variables are always avaialable in AWS CodeBuild
+CODE_BUILD_ARN = os.environ.get('CODEBUILD_BUILD_ARN')
+CODE_BUILD_REGION = os.environ.get('AWS_DEFAULT_REGION')
+CODEBUILD_BUILD_SUCCEEDING = int(os.environ.get('CODEBUILD_BUILD_SUCCEEDING', 0))
+SOURCE_VERSION = os.environ.get('CODEBUILD_RESOLVED_SOURCE_VERSION')
+BUILD_NAME = CODE_BUILD_ARN.split(':')[-2].split('/')[1]
+BUILD_ID = CODE_BUILD_ARN.split(':')[-1]
+BUILD_EMOJI = ':thumbsup:' if CODEBUILD_BUILD_SUCCEEDING else ':bangbang:'
+
+# Gitlab vars
+GITLAB_BASE_URL = 'https://gitlab.intranet.solarmosaic.com'
+GITLAB_API_URL = os.path.join(GITLAB_BASE_URL, 'api/v4/projects')
+
+def get_code_build_url():
     return (
         'https://console.aws.amazon.com/codebuild/home?region={region}'
-        '#/builds/{build_name}:{build_id}/view/new'
-        ).format(
-            region=region, build_name=build_name, build_id=build_id)
+        '#/builds/{build_name}:{build_id}/view/new').format(
+            region=CODE_BUILD_REGION, build_name=BUILD_NAME, build_id=BUILD_ID)
 
-def _get_code_build_body(code_build_url, build_name, source_version):
+def merge_request_note(emoji):
     body = (
-        '<a href="{code_build_url}">{build_name}</a> &nbsp; &nbsp; :thumbsup: '
+        '<a href="{code_build_url}">{build_name}</a> &nbsp; &nbsp; {emoji} '
         '@ commit {source_version}').format(
-            code_build_url=code_build_url, build_name=build_name,
-            source_version=source_version)
+            code_build_url=get_code_build_url(), build_name=BUILD_NAME,
+            emoji=emoji, source_version=SOURCE_VERSION)
     return urllib.quote(body)
 
-def _get_gitlab_merge_request_approval_notify_url(
-        gitlab_project_id, merge_request_id, code_build_body):
-    return (
-        'https://gitlab.intranet.solarmosaic.com/api/v4/projects/{gitlab_project_id}/'
-        'merge_requests/{merge_request_id}/notes?body={body}').format(
-            gitlab_project_id=gitlab_project_id, merge_request_id=merge_request_id,
-            body=code_build_body)
+def merge_request_note_url(body):
+    return os.path.join(
+        GITLAB_API_URL, GITLAB_PROJECT_ID, 'merge_requests',
+        MERGE_REQUEST_INTERNAL_ID, 'notes?body={0}'.format(body))
 
-def _get_gitlab_merge_request_approval_url(gitlab_project_id, merge_request_id):
-    return (
-        'https://gitlab.intranet.solarmosaic.com/api/v4/projects/{gitlab_project_id}/'
-        'merge_requests/{merge_request_id}/approve').format(
-            gitlab_project_id=gitlab_project_id,
-            merge_request_id=merge_request_id)
+def merge_request_approval_url():
+    return os.path.join(
+        GITLAB_API_URL, GITLAB_PROJECT_ID, 'merge_requests',
+        MERGE_REQUEST_INTERNAL_ID, 'approve')
 
-def _send_gitlab_approval_merge_request_note(code_build_url, gitlab_approval_notify_url):
-    print("Sending gitlab approval merge request note")
-    headers = {'Private-Token': os.environ.get('GITLAB_ACCESS_TOKEN')}
-    print("Gitlab URL: {0}".format(gitlab_approval_notify_url))
-    response = requests.post(gitlab_approval_notify_url, headers=headers)
-    print(code_build_url)
-    print(response)
+class RequestSender(object):
+    def __init__(self, token):
+        self.token = token
+    def send_request(self, url):
+        print('Posting to {0}'.format(url))
+        headers = {'Private-Token': self.token}
+        response = requests.post(url, headers=headers)
+        print(response)
 
-def _execute_merge_request_approval(gitlab_url):
-    print("Executing GitLab merge request approval")
-    headers = {'Private-Token': os.environ.get('GITLAB_ACCESS_TOKEN')}
-    print("Gitlab URL: {0}".format(gitlab_url))
-    response = requests.post(gitlab_url, headers=headers)
-    print(response)
-
-def _generate_ecs_params():
+def generate_ecs_params():
     print("Saving updated ECS parameter file...")
     file_path = os.path.join(
         os.environ.get('CODEBUILD_SRC_DIR'), 't-ecs-params-testing.json'
     )
     ecs_params = _parse_json(file_path)
     print(json.dumps(ecs_params, indent=2))
-
     # Swap out the testing environment for a new merge request specific environment
     ecs_params['Parameters']['Environment'] = os.environ.get('PIPELINE_NAME')
-
     ecs_params_file = open('t-ecs-params-testing.json', 'w')
     ecs_params_file.write(json.dumps(ecs_params, indent=2))
 
@@ -108,36 +104,56 @@ def _parse_json(path):
         print('\nYour JSON is not valid! Did you check trailing commas??\n')
         raise(e)
 
-def main():
-    if isMergeRequest():
-        gitlab_token = os.environ.get('GITLAB_ACCESS_TOKEN')
-        pipeline_name = os.environ.get('PIPELINE_NAME')
-        gitlab_project_id = os.environ.get('GITLAB_PROJECT_ID')
-        merge_request_id = os.environ.get('MERGE_REQUEST_INTERNAL_ID')
+def onBuildJobCompletion():
+    sender = RequestSender(GITLAB_ACCESS_TOKEN)
+    # Approve the merge request from a build perspective
+    sender.send_request(merge_request_approval_url())
+    # Notify GitLab of the approval
+    sender.send_request(merge_request_note_url(merge_request_note(BUILD_EMOJI)))
+    # Generate the ECS CloudFormation stack to create an ECS instance
+    generate_ecs_params()
 
-        code_build_arn = os.environ.get('CODEBUILD_BUILD_ARN')
-        code_build_region = os.environ.get('AWS_DEFAULT_REGION')
-        source_version = os.environ.get('CODEBUILD_RESOLVED_SOURCE_VERSION')
-        build_name = code_build_arn.split(':')[-2].split('/')[1]
-        build_id = code_build_arn.split(':')[-1]
+def onUnitTestJobCompletion():
+    sender = RequestSender(GITLAB_UNIT_TEST_ACCESS_TOKEN)
+    # Approve the merge request from a unit test perspective
+    sender.send_request(merge_request_approval_url())
+    # Notify GitLab of the approval
+    sender.send_request(merge_request_note_url(merge_request_note(BUILD_EMOJI)))
 
-        code_build_url = _get_code_build_url(code_build_region, build_name, build_id)
-        code_build_body = _get_code_build_body(code_build_url, build_name, source_version)
-
-        # Execute GitLab merge request approval
-        gitlab_approval_execution_url = _get_gitlab_merge_request_approval_url(
-            gitlab_project_id, merge_request_id)
-        _execute_merge_request_approval(gitlab_approval_execution_url)
-
-        # Notify GitLab of the CodeBuild approval
-        gitlab_approval_notify_url = _get_gitlab_merge_request_approval_notify_url(
-            gitlab_project_id, merge_request_id, code_build_body)
-        _send_gitlab_approval_merge_request_note(code_build_url, gitlab_approval_notify_url)
-
-        _generate_ecs_params()
-    else:
-        print('not all environment variable for gitlab notification are present')
-
+def onLintJobCompletion():
+    sender = RequestSender(GITLAB_LINT_ACCESS_TOKEN)
+    # Approve the merge request from a lint perspective
+    sender.send_request(merge_request_approval_url())
+    # Notify GitLab of the approval
+    sender.send_request(merge_request_note_url(merge_request_note(BUILD_EMOJI)))
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        raise SystemExit('You must pass in build, unit-test, or lint as an argument')
+    # Any of the three following conditions may be met if this is a non-merge request build.
+    if not PIPELINE_NAME:
+        raise SystemExit('PIPELINE_NAME environment variable not set. This might be a non merge-request build.')
+    if not GITLAB_PROJECT_ID:
+        raise SystemExit('GITLAB_PROJECT_ID environment variable not set. This might be a non merge-request build.')
+    if not MERGE_REQUEST_INTERNAL_ID:
+        raise SystemExit('MERGE_REQUEST_INTERNAL_ID environment variable not set. This might be a non merge-request build.')
+
+    arg = sys.argv[1]
+
+    if arg == 'build':
+        if not GITLAB_ACCESS_TOKEN:
+            raise SystemExit('No GITLAB_ACCESS_TOKEN environment variable has been set!')
+        print('Running onBuildJobCompletion')
+        onBuildJobCompletion()
+    elif arg == 'unit-test':
+        if not GITLAB_UNIT_TEST_ACCESS_TOKEN:
+            raise SystemExit('No GITLAB_UNIT_TEST_ACCESS_TOKEN environment variable has been set!')
+        print('Running onUnitTestJobCompletion')
+        onUnitTestJobCompletion()
+    elif arg == 'lint':
+        if not GITLAB_LINT_ACCESS_TOKEN:
+            raise SystemExit('No GITLAB_LINT_ACCESS_TOKEN environment variable has been set!')
+        print('Running onLintJobCompletion')
+        onLintJobCompletion()
+    else:
+        raise SystemExit('Unknown argument: {0}'.format(arg))
