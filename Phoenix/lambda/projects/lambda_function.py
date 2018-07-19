fdf80de6029830d500ba00c1a9a46d8904b5693b
Jason DeBolt <jasondebolt@gmail.com>
2018-07-18 21:27:16 -0700

    Fixing 'projects' lambda function to return CloudFormation stacks of the existing role.

--- Phoenix/lambda/projects/lambda_function.py
+++ Phoenix/lambda/projects/lambda_function.py
@@ -4,25 +4,10 @@ import boto3
 PUT = 'PUT'
 GET = 'GET'
 
-sts_client = boto3.client('sts')
-
 def lambda_handler(event, context):
     print(json.dumps(event))
 
-    sts_response = sts_client.assume_role(
-      # LMS 928181911649
-      #RoleArn='arn:aws:iam::714284646049:role/PhoenixRole',
-      RoleArn='arn:aws:iam::928181911649:role/PhoenixRole',
-      RoleSessionName='PhoenixToPhoenixUIAdmins'
-    )
-
-    cloudformation_client = boto3.client(
-      'cloudformation',
-      aws_access_key_id=sts_response['Credentials']['AccessKeyId'],
-      aws_secret_access_key=sts_response['Credentials']['SecretAccessKey'],
-      aws_session_token=sts_response['Credentials']['SessionToken']
-    )
-
+    cloudformation_client = boto3.client('cloudformation')
     cloudformation_response = cloudformation_client.describe_stacks()
     print(json.dumps(cloudformation_response, indent=2, default=str))
 
