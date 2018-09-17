43dc81a617c2f3613fff81f201dc519e0cb1c83b
Jason DeBolt <jasondebolt@gmail.com>
2018-09-17 11:32:43 -0700

    Adding phx.sh script to allow all local builds to run on the same Docker image that gets run on AWS CodeBuild

---
+++ Phoenix/phx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Allows Phoenix commands/script to be executed on the same Docker container that your CodeBuild jobs use.
+
+# Create the following bash alias in your ~/.bash_profile:
+#   alias phx="./phx.sh"
+
+# USAGE:
+#  phx {any command} {any args}
+
+# EXAMPLES:
+#  phx aws s3 ls
+#  phx ./deploy-database-dev.sh update
+#  phx python generage_dev_params.py {your username}
+#  phx python search_and_replace.py . {search string} {replace string}
+
+# Check if jq utility is installed
+package_name=jq
+t=`which $package_name`
+[ -z "$t" ] && echo "the $package_name isn't installed! Install with 'brew install jq'" && exit 1
+
+IMAGE_ID=$(jq -r '.Parameters.CodeBuildDockerImage' template-macro-params.json)
+#$(aws ecr get-login --no-include-email --region us-east-1) && docker pull $IMAGE_ID
+
+docker run --privileged -it \
+    --workdir /root/phx \
+    -v ~/.aws:/root/.aws \
+    -v $PWD:/root/phx $IMAGE_ID $@
