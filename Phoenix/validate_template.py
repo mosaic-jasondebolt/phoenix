a4b20107b7ebc7837f7a7652635ab63ccc444ef1
Kyle Florence <kyle.florence@joinmosaic.com>
2018-10-11 17:15:56 -0700

    Retry with s3 upload if template body is too large

--- Phoenix/validate_template.py
+++ Phoenix/validate_template.py
@@ -1,5 +1,8 @@
 import glob
-from subprocess import call
+import subprocess
+import boto3
+import time
+import os
 
 
 def main():
@@ -9,7 +12,39 @@ def main():
     for file_path in glob.iglob('./*.json'):
         if "params" not in file_path:
             print('Validating file: {0}'.format(file_path))
-            call(['aws', 'cloudformation', 'validate-template', '--template-body', 'file://{0}'.format(file_path)])
+
+            try:
+                subprocess.check_output([
+                    'aws',
+                    'cloudformation', 'validate-template', '--template-body',
+                    'file://{0}'.format(file_path)
+                ], stderr=subprocess.STDOUT)
+
+            except subprocess.CalledProcessError as e:
+                output = str(e.output)
+
+                # If the template body is too large, it will throw an error, so instead we will need to first upload
+                # the file to s3 and then validate it from there.
+                if 'Member must have length less than or equal to 51200' in output:
+                    print('File {0} is too large, uploading to s3 for validation'.format(file_path))
+
+                    timestamp = str(time.time())
+                    abs_path = os.path.abspath(file_path)
+                    s3_file_name = timestamp + '-' + os.path.basename(abs_path)
+                    s3_path = 'mosaic-phoenix-code-pipeline/{0}'.format(s3_file_name)
+
+                    # upload the file
+                    subprocess.call(['aws', 's3', 'cp', os.path.abspath(file_path), 's3://{0}'.format(s3_path)])
+
+                    # validate the file
+                    subprocess.call([
+                        'aws',
+                        'cloudformation', 'validate-template', '--template-url',
+                        'https://s3.amazonaws.com/{0}'.format(s3_path)
+                    ])
+
+                    # delete the file
+                    subprocess.call(['aws', 's3', 'rm', 's3://{0}'.format(s3_path)])
         else:
             print('Not validating parameters file: {0}'.format(file_path))
 
