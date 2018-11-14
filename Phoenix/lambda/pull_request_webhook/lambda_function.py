53b2bcd8c387fc999b1633d5381e491347bff1e5
Jason DeBolt <jason.debolt@joinmosaic.com>
2018-11-13 17:11:31 -0800

    Adding SecretsManager secret for GitHub pull request secret.
    - Removing custom lambda auth as GitHub has it's own HMAC based auth.
    - Making pull request lambda function use AWS_PROXY since the HMAC encoding
      requires that the lambda function have access to the original, untouched
      GitHub webhook request.

--- Phoenix/lambda/pull_request_webhook/lambda_function.py
+++ Phoenix/lambda/pull_request_webhook/lambda_function.py
@@ -6,6 +6,7 @@ import requests
 from datetime import datetime
 import urllib.parse
 import hmac
+import base64
 
 # Handles GitHub merge request events
 
@@ -17,14 +18,26 @@ ssm_client = boto3.client('ssm')
 
 def lambda_handler(event, context):
     print(event)
+
+    # I used Lambda proxy integration here.
     github_signature = event['headers']['X-Hub-Signature']
-    signature = hmac.new('test123'.encode(), json.dumps(event['body']).encode(), "sha1")
+    signature = hmac.new('test123'.encode(), event['body'].encode(), "sha1")
     expected_signature = 'sha1=' + signature.hexdigest()
 
     print(github_signature)
     print(expected_signature)
 
     if not hmac.compare_digest(github_signature, expected_signature):
-        return {"status": "401"}
+        return {
+            "isBase64Encoded" : "false",
+            "statusCode": "401",
+            "headers": {},
+            "body": "Unauthorized!"
+        }
     else:
-        return {"status": "200"}
+        return {
+            "isBase64Encoded" : "false",
+            "statusCode": "200",
+            "headers": {},
+            "body": "Success!"
+        }
