2cf83f696cd65de29de2b04dd19efe9e6f96795e
Jason DeBolt <jason.debolt@joinmosaic.com>
2018-11-20 14:45:26 -0800

    Adding create/update/delete stack funtionality to the release lambda.

--- Phoenix/lambda/release_webhook/lambda_function.py
+++ Phoenix/lambda/release_webhook/lambda_function.py
@@ -60,6 +60,42 @@ def get_release_environments():
     return [val.strip() for val in result.split(',')]  # ['staging', 'qa', ...]
 
 
+def create_or_update_stack(create, stack_name, template_url, parameters):
+    if create:
+        response = cloudformation_client.create_stack(
+          StackName=stack_name,
+          TemplateURL=template_url,
+          Parameters=parameters,
+          Capabilities=['CAPABILITY_IAM']
+        )
+    else:
+        response = cloudformation_client.update_stack(
+          StackName=stack_name,
+          TemplateURL=template_url,
+          Parameters=parameters,
+          Capabilities=['CAPABILITY_IAM']
+        )
+
+
+def delete_stack_if_exists(stack_name):
+    # Idempotent way to delete a CloudFormation stack.
+    try:
+        print('Attempting to delete stack {0}'.format(stack_name))
+        response = cloudformation_client.delete_stack(StackName=stack_name)
+        return response
+    except Exception as e:
+        print('Error: {0}'.format(e))
+        print('Stack {0} may not exist. Skipping delete.'.format(stack_name))
+
+
+def success_response():
+    return {
+        "isBase64Encoded" : "false",
+        "statusCode": "200",
+        "headers": {},
+        "body": "Success!"
+    }
+
 def lambda_handler(event, context):
     print(event)
 
@@ -97,12 +133,79 @@ def lambda_handler(event, context):
             raise Exception('Object is not a push request!')
 
         repo_name = body.repository.name
+        _, ref_type, ref_name = body.ref.split("/")
+        is_tag, is_branch = ref_type == "tags", ref_type == "heads"
+        if not is_branch:
+            return success_response()
 
         is_create_branch_push_event = body.before == '0000000000000000000000000000000000000000'
         is_delete_branch_push_event = body.after == '0000000000000000000000000000000000000000'
         print('is_create_branch_push_event: ', is_create_branch_push_event)
         print('is_delete_branch_push_event: ', is_delete_branch_push_event)
-        
+
+        # Branch name is the 'ref_name'
+        is_release_branch = bool(re.match('release-\d{8}$', ref_name))
+        print('is_release_branch: ', is_release_branch)
+
+        if not is_release_branch:
+            # We only care about release branches.
+            return success_response()
+
+        release_environments = get_release_environments()
+
+        for release_environment in release_environments:
+            stack_name = '{repo_name}-{release_environment}-{source_branch}'.format(
+                repo_name=repo_name, release_environment=release_environment,
+                source_branch=ref_name.replace('_', '-')
+            )
+
+            template_name = 'template-release-environment-pipeline.json'
+            template_url = 'https://s3.amazonaws.com/{0}/cloudformation/{1}'.format(
+                get_microservice_bucket_name(), template_name)
+
+            parameters=[
+                {'ParameterKey': 'ProjectName', 'ParameterValue': os.environ['PROJECT_NAME']},
+                {'ParameterKey': 'ProjectDescription', 'ParameterValue': os.environ['PROJECT_DESCRIPTION']},
+                {'ParameterKey': 'CodePipelineBucketName', 'ParameterValue': os.environ['CODE_PIPELINE_BUCKET_NAME']},
+                {'ParameterKey': 'CodeBuildDockerImage', 'ParameterValue': os.environ['CODE_BUILD_DOCKER_IMAGE']},
+                {'ParameterKey': 'CodeBuildServiceRoleArn', 'ParameterValue': os.environ['CODE_BUILD_SERVICE_ROLE_ARN']},
+                {'ParameterKey': 'CodePipelineServiceRoleArn', 'ParameterValue': os.environ['CODE_PIPELINE_SERVICE_ROLE_ARN']},
+                {'ParameterKey': 'LambdaBucketName', 'ParameterValue': os.environ['LAMBDA_BUCKET_NAME']},
+                {'ParameterKey': 'PipelineName', 'ParameterValue': stack_name},
+                {'ParameterKey': 'GitHubOrganization', 'ParameterValue': os.environ['GITHUB_ORGANIZATION']},
+                {'ParameterKey': 'RepoName', 'ParameterValue': repo_name},
+                {'ParameterKey': 'SourceBranch', 'ParameterValue': ref_name},
+                {'ParameterKey': 'ReleaseEnvironment', 'ParameterValue': release_environment},
+                {'ParameterKey': 'Token', 'ParameterValue': get_github_access_token()},
+                {'ParameterKey': 'IAMRole', 'ParameterValue': os.environ['IAM_ROLE']}
+            ]
+
+            if is_delete_branch_push_event:
+                print('Deleting Pipeline Stack')
+                # Delete the pipeline stack
+                response = delete_stack_if_exists(stack_name)
+                print(response)
+            else:
+                print('Creating or Updating stack')
+                print('Attempting to create stack')
+                try:
+                    create_or_update_stack(
+                        create=True, stack_name=stack_name, template_url=template_url,
+                        parameters=parameters)
+            except Exception as e:
+                print('Stack may have already been created, and that is OK.')
+                print('Error: {0}'.format(e))
+                print('Attempting to update stack')
+                try:
+                  create_or_update_stack(
+                      create=False, stack_name=stack_name, template_url=template_url,
+                      parameters=parameters)
+                except Exception as ex:
+                    print('Stack may not need to be updated.')
+                    print('Error: {0}'.format(ex))
+                    print('Continuing as normal.')
+
+
         return {
             "isBase64Encoded" : "false",
             "statusCode": "200",
