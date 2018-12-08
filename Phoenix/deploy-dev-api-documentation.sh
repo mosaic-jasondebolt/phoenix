3e867604bf8aa95fe72db7676a83b750f03efc0d
Jason DeBolt <jason.debolt@joinmosaic.com>
2018-12-07 23:17:12 -0800

    Adding VersionId to the API documentation template to enable s3 bucket cleanup.

--- Phoenix/deploy-dev-api-documentation.sh
+++ Phoenix/deploy-dev-api-documentation.sh
@@ -21,19 +21,37 @@ fi
 OP=$(echo $1 | tr '/a-z/' '/A-Z/')
 
 CLOUDFORMATION_ROLE=$(jq -r '.Parameters.IAMRole' template-ssm-globals-macro-params.json)
+ORGANIZATION_NAME=$(jq -r '.Parameters.OrganizationName' template-ssm-globals-macro-params.json)
 PROJECT_NAME=$(jq -r '.Parameters.ProjectName' template-ssm-globals-macro-params.json)
+LAMBDA_BUCKET_NAME=$ORGANIZATION_NAME-$PROJECT_NAME-lambda
 
 listOfVersions='v0'
 for docVersion in $listOfVersions
 do
   ENVIRONMENT=$(jq -r '.Parameters.Environment' template-api-documentation-$docVersion-params-dev.json)
   STACK_NAME=$PROJECT_NAME-api-documentation-$docVersion-$ENVIRONMENT
-  ENVIRONMENT='all'
-  VERSION_ID=$ENVIRONMENT-`date '+%Y-%m-%d-%H%M%S'`
+  VERSION_ID=$ENVIRONMENT-$docVersion-`date '+%Y-%m-%d-%H%M%S'`
   CHANGE_SET_NAME=$VERSION_ID
 
+  # Upload the Python Lambda functions
+  listOfPythonLambdaFunctions='delete_s3_files'
+  for functionName in $listOfPythonLambdaFunctions
+  do
+    mkdir -p builds/$functionName
+    cp -rf lambda/$functionName/* builds/$functionName/
+    cd builds/$functionName/
+    pip install -r requirements.txt -t .
+    zip -r lambda_function.zip ./*
+    aws s3 cp lambda_function.zip s3://$LAMBDA_BUCKET_NAME/$VERSION_ID/$functionName/
+    cd ../../
+    rm -rf builds
+  done
+
+  # Replace the VERSION_ID string in the dev params file with the $VERSION_ID variable
+  sed "s/VERSION_ID/$VERSION_ID/g" template-api-documentation-$docVersion-params-dev.json > temp1.json
+
   # Regenerate the dev params file into a format the the CloudFormation CLI expects.
-  python parameters_generator.py template-api-documentation-$docVersion-params-dev.json cloudformation > temp1.json
+  python parameters_generator.py temp1.json cloudformation > temp2.json
 
   # Make macro name unique in the AWS account:
   # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cloudformation-macro.html#cfn-cloudformation-macro-name
@@ -44,7 +62,7 @@ do
   aws cloudformation create-change-set --stack-name $STACK_NAME \
       --change-set-name $CHANGE_SET_NAME \
       --template-body file://temp0.json \
-      --parameters file://temp1.json \
+      --parameters file://temp2.json \
       --change-set-type $OP \
       --capabilities CAPABILITY_IAM \
       --role-arn $CLOUDFORMATION_ROLE
